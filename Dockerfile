9afba5f275547950c487ac682e7c8a49dbdf5180
Matt Olan <olanmatt@gmail.com>
2020-12-13 08:29:02 -0500

    added tqdm progress bars

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM python:3
 
+RUN pip3 --no-cache-dir install tqdm
 COPY same.py /
 
 ENTRYPOINT ["python3", "same.py"]
--- same.py
+++ same.py
@@ -1,17 +1,21 @@
 import argparse
 import hashlib
-import os
 import io
+import os
+import sys
+from tqdm import tqdm
 
 
 def hash_directory(path):
     results = {}
     for root, _, filenames in os.walk(path):
-        for filename in filenames:
+        for filename in tqdm(
+            filenames, file=sys.stderr, desc="hashing {}".format(path)
+        ):
             full_filename = os.path.join(root, filename)
             hasher = hashlib.sha512()
             with open(full_filename, "rb") as file_data:
-                for block in iter(lambda: file_data.read(io.DEFAULT_BUFFER_SIZE), b''):
+                for block in iter(lambda: file_data.read(io.DEFAULT_BUFFER_SIZE), b""):
                     hasher.update(block)
             results.setdefault(hasher.hexdigest(), []).append(full_filename)
     return results
